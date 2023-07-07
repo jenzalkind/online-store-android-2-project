442d6be3c87c97d539ec81b2d41d46f060aea6a3
jenzalkind <jen.zalkind@gmail.com>
2023-07-07 12:37:20 +0300

    Adding login and signup fragments

---
+++ app/src/main/java/com/example/online_store/LoginFrag.kt
@@ -0,0 +1,23 @@
+package com.example.online_store
+
+import android.app.AlertDialog
+import android.app.Notification
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Handler
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+//import com.example.online_store.databinding.FragmentLoginBinding
+//import com.google.firebase.auth.FirebaseAuth
+
+class LoginFrag : Fragment() {
+}
---
+++ app/src/main/java/com/example/online_store/SignupFrag.kt
@@ -0,0 +1,4 @@
+package com.example.online_store
+
+class SignupFrag {
+}
